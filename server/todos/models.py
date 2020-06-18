e26233ce96bb01ef6d3a0c472c296f70cd95ee43
andres vega <elb.andres8@gmail.com>
2020-06-17 23:34:49 -0500

    descripción es opcional

--- server/todos/models.py
+++ server/todos/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 # Create your models here.
 class ToDo(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=255, default='')
+    description = models.CharField(max_length=255, default='', blank=True, null=True)
     complete = models.BooleanField(default=False)
     create_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
